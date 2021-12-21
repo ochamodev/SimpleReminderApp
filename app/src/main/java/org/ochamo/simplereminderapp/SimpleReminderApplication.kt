d173ce6f2c64c4eb48954c57ffd5a7f2b7315fc9
Otto Chamo <ochamo@develsystems.com>
2021-12-20 20:36:54 -0600

    chore: Create Application class

---
+++ app/src/main/java/org/ochamo/simplereminderapp/SimpleReminderApplication.kt
@@ -0,0 +1,8 @@
+package org.ochamo.simplereminderapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class SimpleReminderApplication : Application() {
+}
